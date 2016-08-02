712f78f94f8a273b2e339a30c31138bfef599b1f
Joshua Rich <joshua.rich@gmail.com>
2016-08-02 22:24:23 +1000

    Add command-line parsing to puppet-apply script.  Modify main init.pp to use new FACTER_ variables.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,9 @@
 define bash::config() {
-  file { "$::home/${name}":
+  file { "$::myhome/${name}":
     ensure    => file,
     source    => "puppet:///modules/bash/${name}",
-    owner     => $::user,
-    group     => $::group,
+    owner     => $::myuser,
+    group     => $::mygroup,
     mode      => '0644',
     show_diff => true
   }
@@ -19,23 +19,23 @@ bash::config { ['.bashrc',
 
 exec { 'dircolors':
   command => "/usr/bin/curl -L https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.256dark -o ~/.dircolors",
-  creates => "$::home/.dircolors"
+  creates => "$::myhome/.dircolors"
 }
 
-file { "$::home/.tmux.conf":
+file { "$::myhome/.tmux.conf":
   ensure    => file,
   source    => 'puppet:///modules/tmux/.tmux.conf',
-  owner     => $::user,
-  group     => $::group,
+  owner     => $::myuser,
+  group     => $::mygroup,
   mode      => '0644',
   show_diff => true
 }
 
-file { "$::home/.emacs":
+file { "$::myhome/.emacs":
   ensure    => file,
   source    => 'puppet:///modules/emacs/.emacs',
-  owner     => $::user,
-  group     => $::group,
+  owner     => $::myuser,
+  group     => $::mygroup,
   mode      => '0644',
   show_diff => true
 }
