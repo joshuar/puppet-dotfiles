478e50fd22d395f9137560402765d3e5b241811c
Joshua Rich <joshua.rich@gmail.com>
2017-04-30 22:39:30 +1000

    Less machine dependent scripts.

--- modules/argos/files/ping.sh
+++ modules/argos/files/ping.sh
@@ -28,7 +28,7 @@ COLORS=(${RED_GREEN_THEME[@]})
 MENUFONT="" #size=10 font=UbuntuMono-Bold"
 FONT=""
 MAX_PING=1000
-SITES=(google.com.au astralcars.com ttrss.astralcars.com github.com reddit.com twitter.com)
+SITES=(google.com.au github.com reddit.com twitter.com)
 
 #grab ping times for all sites
 SITE_INDEX=0
