73b842569ff57f8e7be95018a5efeda70f423b9d
Joshua Rich <joshua.rich@gmail.com>
2017-08-14 20:53:26 +1000

    Fix directory permissions.

--- modules/argos/manifests/init.pp
+++ modules/argos/manifests/init.pp
@@ -3,7 +3,7 @@ class argos {
     ensure => directory,
     owner  => $::myuser,
     group  => $::mygroup,
-    mode   => '0640'
+    mode   => '0700'
 }
   argos::script { ['bandwidth','sysstat']:
     duration => '2s'
