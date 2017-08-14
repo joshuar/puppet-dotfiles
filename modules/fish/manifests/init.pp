30f9c67502e439a8f3cbd34ec85995abba884412
Joshua Rich <joshua.rich@gmail.com>
2017-08-14 20:46:37 +1000

    Create .config/fish if needed.

--- modules/fish/manifests/init.pp
+++ modules/fish/manifests/init.pp
@@ -1,5 +1,11 @@
 # Class fish is for the fish shell
 class fish {
+  file { "${::myhome}/.config/fish":
+    ensure => directory,
+    owner  => $::myuser,
+    group  => $::mygroup,
+    mode   => '0640',
+  }
   file { "${::myhome}/.config/fish/config.fish":
     ensure    => file,
     source    => 'puppet:///modules/fish/config.fish',
