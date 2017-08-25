918331d6be556acba5109324edb84d4dd8f539bf
Joshua Rich <joshua.rich@elastic.co>
2017-08-25 18:28:27 +1000

    Add installation of cpuacct and micro Go packages.

--- modules/golang/manifests/init.pp
+++ modules/golang/manifests/init.pp
@@ -2,4 +2,21 @@ class golang {
   file { "$::myhome/gocode":
     ensure => 'directory',
   }
+  exec { 'go-get-micro':
+    environment => "GOPATH=${::myhome}/gocode",
+    command => '/usr/bin/go get  -d github.com/zyedidia/micro/cmd/micro',
+    creates => "${::myhome}/gocode/src/github.com/zyedidia/micro"
+  }
+  exec { 'install-micro':
+    environment => "GOPATH=${::myhome}/gocode",
+    cwd => "${::myhome}/gocode/src/github.com/zyedidia/micro",
+    command => '/usr/bin/make install',
+    creates => "${::myhome}gocode/bin/micro",
+    require => Exec['go-get-micro']
+  }
+  exec { 'go-get-cpuacct':
+    environment => "GOPATH=${::myhome}/gocode",
+    command => '/usr/bin/go get github.com/uber-common/cpustat',
+    creates => "${::myhome}/gocode/bin/cpuacct"
+  }
 }
