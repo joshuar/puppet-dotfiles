324b0953b5db53c36fe1f72e8e142105d94b4464
Joshua Rich <joshua.rich@gmail.com>
2018-02-20 22:59:13 +1100

    Remove micro Golang package being installed by go get.

--- modules/golang/manifests/init.pp
+++ modules/golang/manifests/init.pp
@@ -1,18 +1,6 @@
 class golang {
-  file { "$::myhome/gocode":
+  file { "${::myhome}/gocode":
     ensure => 'directory',
-  }
-  exec { 'go-get-micro':
-    environment => "GOPATH=${::myhome}/gocode",
-    command => '/usr/bin/go get -d github.com/zyedidia/micro/cmd/micro',
-    creates => "${::myhome}/gocode/src/github.com/zyedidia/micro"
-  }
-  exec { 'install-micro':
-    environment => "GOPATH=${::myhome}/gocode",
-    cwd => "${::myhome}/gocode/src/github.com/zyedidia/micro",
-    command => '/usr/bin/make install',
-    creates => "${::myhome}/gocode/bin/micro",
-    require => Exec['go-get-micro']
   }
 
   golang::get { 'cpustat':
